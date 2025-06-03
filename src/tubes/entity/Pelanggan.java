fca014a7a646c58d70de7323d24871143693ee8f
Yoan <145577127+Yoannn13@users.noreply.github.com>
2025-06-03 23:02:53 +0700

    Feat : menambahkan kelas pelanggan

---
+++ src/tubes/entity/Pelanggan.java
@@ -0,0 +1,28 @@
+package tubes.entity;
+
+public class Pelanggan {
+    private String nama;
+    private static int counter = 1;
+    private int idAntrean;
+
+    public Pelanggan(String nama) {
+        this.nama = nama;
+        this.idAntrean = counter++;
+    }
+
+    public String getNama() {
+        return nama;
+    }
+
+    public int getIdAntrean() {
+        return idAntrean;
+    }
+
+    public void setIdAntrean(int idAntrean) {
+        this.idAntrean = idAntrean;
+    }
+
+    public String toString() {
+        return "ID Antrean: " + idAntrean + ", Nama Pelanggan: " + nama;
+    }
+}
