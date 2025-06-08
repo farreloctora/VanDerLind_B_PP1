42e9c06a52396d99b6b0aa485b872255f4234181
SmokeDaPen <145580540+JenasAldera@users.noreply.github.com>
2025-06-08 23:44:23 +0700

    Feat: Menambahkan fitur cancelById

--- src/tubes/services/SturkturQueue.java
+++ src/tubes/services/SturkturQueue.java
@@ -25,24 +25,26 @@ public class StrukturQueue {
         FileUtil.saveToFile(this);
     }
 
-    public void dequeue() {
+    //Not yet committed update
+    public Pelanggan dequeue() {
         if (isEmpty()) {
-            System.out.println("Antrean kosong, tidak ada yang bisa diproses.");
+            return null;
         } else {
-            System.out.println("Memproses: " + front.getData().toString());
+            Pelanggan data = front.getData();
             front = front.getNext();
             if (front == null) {
                 rear = null;
             }
             FileUtil.saveToFile(this);
+            return data;
         }
     }
 
     public void display() {
         if (isEmpty()) {
-            System.out.println("Antrean kosong.");
+            System.out.println("\nAntrean kosong.");
         } else {
-            System.out.println("=== Daftar Pelanggan dalam Antrean ===");
+            System.out.println("\n=== Daftar Pelanggan dalam Antrean ===");
             Node current = front;
             int no = 1;
             while (current != null) {
@@ -63,11 +65,36 @@ public class StrukturQueue {
         return count;
     }
 
+    public boolean cancelById(int id) {
+        if (isEmpty()) {
+            return false; 
+        }
+        if (front.getData().getIdAntrean() == id) { 
+            dequeue(); 
+            return true; 
+        }
+        Node prev = front;
+        Node curr = front.getNext();
+        while (curr != null) {
+            if (curr.getData().getIdAntrean() == id) { 
+                prev.setNext(curr.getNext()); 
+                if (curr == rear) {
+                    rear = prev; 
+                }
+                FileUtil.saveToFile(this); 
+                return true; 
+            }
+            prev = curr;
+            curr = curr.getNext();
+        }
+        return false; 
+    }
+    
     public void resetQueue() {
         front = null;
         rear = null;
         Pelanggan.resetCounter();
-        System.out.println("Antrean telah direset.");
+        System.out.println("\nAntrean telah direset."); 
         FileUtil.saveToFile(this);
     }
 } 
